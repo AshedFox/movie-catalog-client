e1e058ae09fb35cb5fcb11e644b6853add6b6039
AshedFox <a.mahn@mail.ru>
2023-04-03 01:32:46 +0300

    fix export of non-existent component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,6 @@
-export { default as Select } from './Select';
 export { default as Field } from './Field';
 export { default as Button } from './Button';
 export { default as Avatar } from './Avatar';
-export { default as Carousel } from './CarouselBackup';
+export { default as Carousel } from './Carousel';
 export { default as Loader } from './Loader';
 export { default as Modal } from './Modal';
