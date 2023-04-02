87a00ade694c3cd6ab73b7e50f039ae7197ce54e
AshedFox <a.mahn@mail.ru>
2023-04-02 23:36:02 +0300

    add base UI elements
    Add Avatar, Button, Carousel, Field, MultilineField and Loader UI elements.

---
+++ src/components/ui/MultilineField.tsx
@@ -0,0 +1,43 @@
+import React, { ComponentPropsWithoutRef, forwardRef } from 'react';
+import clsx from 'clsx';
+
+type Props = {
+  id?: string;
+  name: string;
+  label: string;
+  error?: string;
+} & ComponentPropsWithoutRef<'textarea'>;
+
+const MultilineField = forwardRef<HTMLTextAreaElement, Props>(
+  (
+    { id, label, name, error, required, onChange, ...otherProps }: Props,
+    ref,
+  ) => (
+    <div>
+      <label className="text-sm font-medium mb-2" htmlFor={id ?? name}>
+        {label}
+        {required && <span className="text-sm text-red-500">*</span>}
+      </label>
+      <textarea
+        id={id}
+        ref={ref}
+        className={clsx(
+          'appearance-none resize-none rounded w-full py-2 px-3 leading-tight',
+          'bg-gray-50 text-gray-700 dark:bg-gray-700 dark:text-gray-100',
+          'border border-gray-200 dark:border-gray-600',
+          'focus:outline-none',
+          {
+            'border-red-500': error,
+          },
+        )}
+        name={name}
+        {...otherProps}
+      />
+      {error && <p className="text-xs text-red-500 italic">{error}</p>}
+    </div>
+  ),
+);
+
+MultilineField.displayName = 'MultilineField';
+
+export default MultilineField;
---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as Select } from './Select';
+export { default as Field } from './Field';
+export { default as Button } from './Button';
+export { default as Avatar } from './Avatar';
+export { default as Carousel } from './CarouselBackup';
+export { default as Loader } from './Loader';
+export { default as Modal } from './Modal';
