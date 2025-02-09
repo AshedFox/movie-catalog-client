cd4e6e1344fdc212b4326a5bd8c78274bccda49c
AshedFox <a.mahn@mail.ru>
2025-02-09 04:22:02 +0300

    add NEXT_PUBLIC to some .env variables

--- src/features/auth/constants.ts
+++ src/features/auth/constants.ts
@@ -1,2 +1,2 @@
-export const AUTH_BASE_URL = process.env.AUTH_URL ?? 'http://localhost:3001';
-export const AUTH_SESSION_COOKIE_KEY = process.env.SESSION_COOKIE_KEY ?? 'auth.session';
+export const AUTH_BASE_URL = process.env.NEXT_PUBLIC_AUTH_URL ?? 'http://localhost:3001';
+export const AUTH_SESSION_COOKIE_KEY = process.env.NEXT_PUBLIC_SESSION_COOKIE_KEY ?? 'auth.session';
